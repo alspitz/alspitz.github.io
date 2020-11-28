2642d68408994d36d4e06dde0dd48a90b418f64b
Alex Spitzer <aes368@cornell.edu>
2020-11-28 03:50:00 -0500

    Add header

---
+++ header.js
@@ -0,0 +1,9 @@
+let header = `
+<a class="headerlink" href="index.html">Home</a>
+
+<a class="headerlink" href="quadsim.html">Simulation</a>
+
+<a class="headerlink" href="index.html#papers">Papers</a>
+`;
+
+document.getElementById("header").innerHTML = header;
