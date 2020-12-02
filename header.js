93f6a530a6b5785a488b4c77bfb88c1ab92a141f
Alex Spitzer <aes368@cornell.edu>
2020-12-01 20:24:06 -0500

    Update all to be more responsive except for quadsim

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 let header = `
-<a class="headerlink" href="index.html">Home</a>
-
-<a class="headerlink" href="quadsim.html">Simulation</a>
-
-<a class="headerlink" href="index.html#papers">Papers</a>
+<div class="headerdivinner">
+  <a class="headerlink" href="index.html">Home</a>
+  <a class="headerlink" href="quadsim.html">Simulation</a>
+  <a class="headerlink" href="index.html#papers">Papers</a>
+</div>
 `;
 
 document.getElementById("header").innerHTML = header;
