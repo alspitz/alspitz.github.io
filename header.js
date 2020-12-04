aa0e9c8b972970366e9a53be2c4dba25049dfcbe
Alex Spitzer <aes368@cornell.edu>
2020-12-04 02:53:13 -0500

    Don't explicitly reference index.html in header

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
 let header = `
 <div class="headerdivinner">
-  <a class="headerlink" href="index.html">Home</a>
-  <a class="headerlink" href="quadsim.html">Simulation</a>
-  <a class="headerlink" href="index.html#papers">Papers</a>
+  <a class="headerlink" href="/">Home</a>
+  <a class="headerlink" href="/quadsim.html">Simulation</a>
+  <a class="headerlink" href="/#papers">Papers</a>
 </div>
 `;
 
